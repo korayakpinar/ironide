e19e45a7f2b0937932aa93da71fff4be9f7f24e6
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-29 12:14:15 +0300

    Update AssemblyInfo of testapp.

--- test-app/Properties/AssemblyInfo.cs
+++ test-app/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("test-app")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Test application for ironide.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mertcan Davulcu")]
 [assembly: AssemblyProduct("test-app")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Ironide")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
