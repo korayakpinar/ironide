ef8010e1608ff3c0986b2f5db4f3c468986b06b6
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 11:39:43 +0300

    Add Description attribute to IIronideControl interface.

--- src/IIronideControl.cs
+++ src/IIronideControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
 
 namespace Ironide {
     /// <summary>
@@ -10,21 +11,25 @@ namespace Ironide {
         /// <summary>
         /// Color of background.
         /// </summary>
+        [Description("Color of background.")]
         Color BackColor { get; set; }
 
         /// <summary>
         /// Color of foreground.
         /// </summary>
+        [Description("Color of foreground.")]
         Color ForeColor { get; set; }
 
         /// <summary>
         /// Color of border.
         /// </summary>
+        [Description("Color of border.")]
         Color BorderColor { get; set; }
 
         /// <summary>
         /// Thickness of border.
         /// </summary>
+        [Description("Thickness of boder.")]
         int BorderThickness { get; set; }
 
         #endregion
