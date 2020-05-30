a124fa6974948fbd08644c0ea062cfc3f4c16089
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 12:03:49 +0300

    General update.

--- src/IronideControl.cs
+++ src/IronideControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Ironide {
@@ -6,9 +7,25 @@ namespace Ironide {
     /// Control template of Ironide.
     /// </summary>
     public abstract class IronideControl:Control, IIronideControl {
+        #region Drawing
+
+        protected override void OnPaintBackground(PaintEventArgs e) {
+            using(var bgbrush = new SolidBrush(BackColor))
+                e.Graphics.FillRectangle(bgbrush,ClientRectangle);
+
+            ControlPaint.DrawBorder(e.Graphics,ClientRectangle,BorderColor,
+                BorderThickness,ButtonBorderStyle.Solid,BorderColor,BorderThickness,ButtonBorderStyle.Solid,
+                BorderColor,BorderThickness,ButtonBorderStyle.Solid,BorderColor,
+                BorderThickness,ButtonBorderStyle.Solid);
+        }
+
+        #endregion
+
         #region Properties
 
         private Color borderColor;
+        [Description("Color of border.")]
+        [DefaultValue(typeof(Color),"DodgerBlue")]
         public virtual Color BorderColor {
             get => borderColor;
             set {
@@ -21,6 +38,8 @@ namespace Ironide {
         }
 
         private int borderThickness;
+        [Description("Thickness of boder.")]
+        [DefaultValue(1)]
         public virtual int BorderThickness {
             get => borderThickness;
             set {
@@ -31,6 +50,20 @@ namespace Ironide {
                 Invalidate();
             }
         }
+
+        /// <summary>
+        /// Color of background.
+        /// </summary>
+        [Description("Color of background.")]
+        [DefaultValue(typeof(Color),"White")]
+        public override Color BackColor { get => base.BackColor; set => base.BackColor=value; }
+
+        /// <summary>
+        /// Color of foreground.
+        /// </summary>
+        [Description("Color of foreground.")]
+        [DefaultValue(typeof(Color),"Black")]
+        public override Color ForeColor { get => base.ForeColor; set => base.ForeColor=value; }
 
         public override string Text {
             get => base.Text;
--- test-app/app.cs
+++ test-app/app.cs
@@ -1,29 +1,15 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
 
 namespace test_app {
-    /// <summary>
-    /// Main window of test application.
-    /// </summary>
     public partial class app:Form {
-        #region Constructors
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
         public app() {
-            Init();
-        }
-
-        #endregion
-    }
-
-    // Designer.
-    public partial class app {
-        /// <summary>
-        /// Initialize component.
-        /// </summary>
-        public void Init() {
-
+            InitializeComponent();
         }
     }
 }
