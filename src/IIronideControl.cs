5ac801495acb81142d80fe578268be36e5d69409
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 11:37:16 +0300

    Add IIronideControl interface.

---
+++ src/IIronideControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Drawing;
+
+namespace Ironide {
+    /// <summary>
+    /// Interface for Ironide controls.
+    /// </summary>
+    public interface IIronideControl {
+        #region Properties
+
+        /// <summary>
+        /// Color of background.
+        /// </summary>
+        Color BackColor { get; set; }
+
+        /// <summary>
+        /// Color of foreground.
+        /// </summary>
+        Color ForeColor { get; set; }
+
+        /// <summary>
+        /// Color of border.
+        /// </summary>
+        Color BorderColor { get; set; }
+
+        /// <summary>
+        /// Thickness of border.
+        /// </summary>
+        int BorderThickness { get; set; }
+
+        #endregion
+    }
+}
