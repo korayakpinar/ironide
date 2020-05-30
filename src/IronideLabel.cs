18c10512658687c20edac24c8dee38eb0828211c
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 14:02:29 +0300

    Add IronideLabel.

---
+++ src/IronideLabel.cs
@@ -0,0 +1,23 @@
+﻿namespace Ironide {
+    /// <summary>
+    /// Label of Ironide.
+    /// </summary>
+    public class IronideLabel:IronideControl {
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IronideLabel() { }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="text">Text of label.</param>
+        public IronideLabel(string text) {
+            Text = text;
+        }
+
+        #endregion
+    }
+}
