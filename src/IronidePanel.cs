1b90ba23666113e588dfc9b4878fe6045cd4576f
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 14:16:18 +0300

    Add IronidePanel.

---
+++ src/IronidePanel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Ironide {
+    /// <summary>
+    /// Panel of Ironide.
+    /// </summary>
+    public class IronidePanel:IronideControl {
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public IronidePanel() {
+
+        }
+
+        #endregion
+
+        #region Drawing
+
+        protected override void OnPaint(PaintEventArgs e) {
+            if(TextRender)
+                DrawText(e.Graphics);
+            DrawEnable(e.Graphics);
+        }
+
+        #endregion
+
+        #region Properties
+
+        private bool textRender = false;
+        /// <summary>
+        /// Render text.
+        /// </summary>
+        [Description("Render text.")]
+        [DefaultValue(false)]
+        public bool TextRender {
+            get => textRender;
+            set {
+                if(value == textRender)
+                    return;
+
+                textRender = value;
+                Invalidate();
+            }
+        }
+
+        #endregion
+    }
+}
