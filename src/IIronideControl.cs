1342d2df658d9822001c0b30f7b65af70527f7b8
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 11:42:54 +0300

    Add DefaultValue attribute to IIronideControl interface.

--- src/IIronideControl.cs
+++ src/IIronideControl.cs
@@ -12,24 +12,28 @@ namespace Ironide {
         /// Color of background.
         /// </summary>
         [Description("Color of background.")]
+        [DefaultValue(typeof(Color),"White")]
         Color BackColor { get; set; }
 
         /// <summary>
         /// Color of foreground.
         /// </summary>
         [Description("Color of foreground.")]
+        [DefaultValue(typeof(Color),"Black")]
         Color ForeColor { get; set; }
 
         /// <summary>
         /// Color of border.
         /// </summary>
         [Description("Color of border.")]
+        [DefaultValue(typeof(Color),"DodgerBlue")]
         Color BorderColor { get; set; }
 
         /// <summary>
         /// Thickness of border.
         /// </summary>
         [Description("Thickness of boder.")]
+        [DefaultValue(1)]
         int BorderThickness { get; set; }
 
         #endregion
