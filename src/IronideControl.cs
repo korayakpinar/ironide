0b7172ec21202ddb8f847a8eebdc418dc36786bc
Mertcan Davulcu <54983926+mertcandav@users.noreply.github.com>
2020-05-30 11:49:16 +0300

    Add IronideControl abstract class.

---
+++ src/IronideControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ironide {
+    /// <summary>
+    /// Control template of Ironide.
+    /// </summary>
+    public abstract partial class IronideControl:Control, IIronideControl {
+        #region Properties
+
+        private Color borderColor;
+        public virtual Color BorderColor {
+            get => borderColor;
+            set {
+                if(value == borderColor)
+                    return;
+
+                borderColor = value;
+                Invalidate();
+            }
+        }
+
+        private int borderThickness;
+        public virtual int BorderThickness {
+            get => borderThickness;
+            set {
+                if(borderThickness == value)
+                    return;
+
+                borderThickness = value;
+                Invalidate();
+            }
+        }
+
+        #endregion
+    }
+}
